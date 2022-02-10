d1991aa6970cc9fa07d5f0274010f172a78b92e8
DiegoBautista <da10bautista@gmail.com>
2022-02-10 08:09:00 -0600

    Creación del proyecto y creación librería de clases

---
+++ Lab02_ed_22/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab02_ed_22.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
